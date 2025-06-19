9111589939547b5fdb5b551063a80d8e34277aae
Cristian Melgoza <cpmelgoza05@gmail.com>
2025-06-19 10:53:08 -0700

    Clean up animator

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -12,7 +12,6 @@ public class Knight : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private Animator animator;
 
-    private bool isIdle = false;
     private bool isGrounded = false;
     private bool isFalling = false;
     private bool isTurning = false;
@@ -33,7 +32,6 @@ public class Knight : MonoBehaviour
         float accelFactor = Mathf.Sign(horizontalInput) == Mathf.Sign(rbody.linearVelocityX) ? 1.0f : 2.0f;
         rbody.linearVelocityX = Mathf.MoveTowards(rbody.linearVelocityX, horizontalInput * runSpeed, accelFactor * runAccel * Time.deltaTime);
 
-        isIdle = horizontalInput == 0.0f;
         isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 0.1f, terrainLayerMask);
         isFalling = !isGrounded && rbody.linearVelocityY < 0.0f;
 
@@ -45,10 +43,24 @@ public class Knight : MonoBehaviour
 
         animator.SetFloat("run speed", Mathf.Abs(rbody.linearVelocityX));
         animator.SetBool("is falling", isFalling);
-        animator.SetBool("is idle", isIdle);
         animator.SetBool("is grounded", isGrounded);
 
-        isTurning = isGrounded && horizontalInput != 0.0f && ((horizontalInput < 0.0f) != spriteRenderer.flipX);
+        if (isGrounded && horizontalInput != 0.0f && ((horizontalInput < 0.0f) != spriteRenderer.flipX))
+        {
+            if (Mathf.Abs(rbody.linearVelocityX) > 2.0f)
+            {
+                isTurning = true;
+            }
+            else
+            {
+                if (!isTurning) spriteRenderer.flipX = !spriteRenderer.flipX;
+            }
+        }
+        else
+        {
+            isTurning = false;
+        }
+
         animator.SetBool("is turning", isTurning);
     }
 }
