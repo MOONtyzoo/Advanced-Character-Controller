11475736bfb63bea897d46db1bfdfca7eee03d96
Cristian M <cpmelgoza05@gmail.com>
2026-03-01 09:28:52 -0800

    Add tutorial text to scene

--- Assets/Plugins/StateMachine/Templates/TemplateRunner.cs
+++ Assets/Plugins/StateMachine/Templates/TemplateRunner.cs
@@ -1,52 +1,44 @@
 using UnityEngine;
+using StateMachine;
 
-// Please read the documentation (in the readme) if this is confusing!
-
-// You can remove this namespace after duplicating the file!
-namespace StateMachine
+public partial class TemplateRunner : MonoBehaviour
 {
-    public partial class TemplateRunner : MonoBehaviour
+    [SerializeField] private bool debugMode = false;
+    
+    private StateMachine<StateKey, TemplateRunner> stateMachine;
+    public enum StateKey
     {
-        [SerializeField] private bool debugMode = false;
+        Idle,
+        Run,
+        Aerial
+    }
+
+    private void Awake()
+    {
+        InitializeStateMachine();
+    }
+
+    private void InitializeStateMachine()
+    {
+        stateMachine = new StateMachine<StateKey, TemplateRunner>();
+        stateMachine.SetDebugMode(debugMode);
+
+        var idleState = new TemplateState(this);
+        stateMachine.AddState(StateKey.Idle, idleState);
         
-        private StateMachine<StateKey, TemplateRunner> stateMachine;
-        public enum StateKey
-        {
-            Idle,
-            Run,
-            Aerial
-        }
+        // Repeat the above two lines for each state you want to add
+        
+        stateMachine.Begin(StateKey.Idle); // DONT FORGET THIS!!!
+    }
 
-        private void Awake()
-        {
-            InitializeStateMachine();
-        }
+    private void Update()
+    {
+        stateMachine.Update();
+    }
 
-        private void InitializeStateMachine()
-        {
-            stateMachine = new StateMachine<StateKey, TemplateRunner>();
-            stateMachine.SetDebugMode(debugMode);
-
-            var idleState = new TemplateState(this);
-            stateMachine.AddState(StateKey.Idle, idleState);
-            
-            var runState = new TemplateState(this);
-            stateMachine.AddState(StateKey.Run, runState);
-            
-            var aerialState = new TemplateState(this);
-            stateMachine.AddState(StateKey.Aerial, aerialState);
-            
-            stateMachine.Begin(StateKey.Idle); // DONT FORGET THIS!!!
-        }
-
-        private void Update()
-        {
-            stateMachine.Update();
-        }
-
-        private void FixedUpdate()
-        {
-            stateMachine.FixedUpdate();
-        }
+    private void FixedUpdate()
+    {
+        stateMachine.FixedUpdate();
     }
 }
+
